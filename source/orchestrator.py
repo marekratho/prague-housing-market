c27db5acf0fef64fc1d354ee34d964f326f73206
Marek Rathousky <Marek@Mareks-MBP.lan>
2023-09-04 22:02:39 +0200

    feature(orchestrator): Add saving of raw listing data

--- source/orchestrator.py
+++ source/orchestrator.py
@@ -2,24 +2,25 @@ from scraper_engine.bezrealitky_scraper import BezrealitkyScraper
 from scraper_engine.sreality_scraper import SrealityScraper
 from scraper_engine.ceskereality_scraper import CeskerealityScraper
 import json
-from utils import print_data_report
+import datetime
+from utils import print_data_report, save_list, load_config
 
 
 if __name__ == "__main__":
-    # with open('./config/bezrealitky_config.json', mode='r') as f:
-    #     config = json.load(f)
-    # bezrealitkyScraper = BezrealitkyScraper(config=config)
-    # bezrealitkyScraper.scrape(total_attemps=10)
-    # print_data_report(bezrealitkyScraper.listings, 'Cena', 'DISPOZICE')
+    bezrealitky_config = load_config('./config/bezrealitky_config.json')
+    bezrealitkyScraper = BezrealitkyScraper(config=bezrealitky_config)
+    bezrealitkyScraper.scrape(total_attemps=10)
+    save_list(bezrealitkyScraper.listings, f"../raw_data/bezrealitky_raw_{datetime.today().strftime('%Y-%m-%d')}.pkl")
+    print_data_report(bezrealitkyScraper.listings, 'Cena', 'DISPOZICE')
 
-    # with open('./config/sreality_config.json', mode='r') as f:
-    #     sreality_config = json.load(f)
-    # srealityScraper = SrealityScraper(config=sreality_config)
-    # srealityScraper.scrape()
-    # srealityScraper.parse_listing_name()
-    # print_data_report(srealityScraper.listings, 'Cena', 'Disposition')
+    sreality_config = load_config('./config/sreality_config.json')
+    srealityScraper = SrealityScraper(config=sreality_config)
+    srealityScraper.scrape()
+    save_list(srealityScraper.listings, f"../raw_data/sreality_raw_{datetime.today().strftime('%Y-%m-%d')}.pkl")
+    srealityScraper.parse_listing_name()
+    print_data_report(srealityScraper.listings, 'Cena', 'Disposition')
 
-    with open('./config/ceskereality_config.json', mode='r') as f:
-        ceskereality_config = json.load(f)
+    ceskereality_config = load_config('./config/ceskereality_config.json')
     ceskerealityScraper = CeskerealityScraper(config=ceskereality_config)
     ceskerealityScraper.scrape()
+    save_list(ceskerealityScraper.listings, f"../raw_data/ceskereality_raw_{datetime.today().strftime('%Y-%m-%d')}.pkl")
