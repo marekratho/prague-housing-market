d48d321fc58b5b3e7ba82f93eaf5c116b97979a8
Marek Rathousky <Marek@Mareks-MBP.lan>
2023-09-04 21:19:07 +0200

    feature(scraper-engine): Add scraper for Ceskereality

--- source/orchestrator.py
+++ source/orchestrator.py
@@ -1,5 +1,6 @@
 from scraper_engine.bezrealitky_scraper import BezrealitkyScraper
 from scraper_engine.sreality_scraper import SrealityScraper
+from scraper_engine.ceskereality_scraper import CeskerealityScraper
 import json
 from utils import print_data_report
 
@@ -11,9 +12,14 @@ if __name__ == "__main__":
     # bezrealitkyScraper.scrape(total_attemps=10)
     # print_data_report(bezrealitkyScraper.listings, 'Cena', 'DISPOZICE')
 
-    with open('./config/sreality_config.json', mode='r') as f:
-        sreality_config = json.load(f)
-    srealityScraper = SrealityScraper(config=sreality_config)
-    srealityScraper.scrape()
-    srealityScraper.parse_listing_name()
-    print_data_report(srealityScraper.listings, 'Cena', 'Disposition')
+    # with open('./config/sreality_config.json', mode='r') as f:
+    #     sreality_config = json.load(f)
+    # srealityScraper = SrealityScraper(config=sreality_config)
+    # srealityScraper.scrape()
+    # srealityScraper.parse_listing_name()
+    # print_data_report(srealityScraper.listings, 'Cena', 'Disposition')
+
+    with open('./config/ceskereality_config.json', mode='r') as f:
+        ceskereality_config = json.load(f)
+    ceskerealityScraper = CeskerealityScraper(config=ceskereality_config)
+    ceskerealityScraper.scrape()
